4ce3082bf4d6e9e396182bd451c4c9f9bf17fd5c
Lisa Feng <lisafeng10@gmail.com>
2020-06-25 17:25:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save()
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    self.all.each {|puppy| puts "#{puppy.name}"}
+  end
+  
+  def save
+    @@all << self
+  end
+  
+end
